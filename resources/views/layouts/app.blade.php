fe606c113881fed66b3a4e2770487d1b3945b4e1
Mukhammadamin2002 <abdullaev2002>
2021-05-31 00:20:39 +0500

    Changes made

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -22,7 +22,7 @@
             <div class="container mx-auto flex justify-between items-center px-6">
                 <div>
                     <a href="{{ url('/') }}" class="text-lg font-semibold text-gray-100 no-underline">
-                        {{ config('app.name', 'My Blog') }}
+                        {{ config('app.name', 'Laravel') }}
                     </a>
                 </div>
                 <nav class="space-x-4 text-gray-300 text-sm sm:text-base">
